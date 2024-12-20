73ea7448edb5ba6e8e336ee685a5ed8eb136b5eb
Fedelattuada <95367090+Fedelattuada@users.noreply.github.com>
2024-12-20 13:40:54 +0100

    fix tabella principale
    fix tabella principale

--- webapp/controller/Master3.controller.js
+++ webapp/controller/Master3.controller.js
@@ -25,16 +25,16 @@ sap.ui.define([
                 this.getOwnerComponent().setModel(oMaster3Model, 'master3');
                 break;
             case "02":
-                this.getView().setModel(new JSONModel({}), 'master3');
+                this.getOwnerComponent().setModel(new JSONModel({}), 'master3');
                 break;
             case "03":
-              
+                this.getOwnerComponent().setModel(new JSONModel({}), 'master3');
                 break;
             case "04":
-              
+                this.getOwnerComponent().setModel(new JSONModel({}), 'master3');
                 break;
             case "05":
-              
+                this.getOwnerComponent().setModel(new JSONModel({}), 'master3');
                 break;
         }
         },
@@ -69,6 +69,15 @@ sap.ui.define([
 
         deletePress: function(oEvent) {
 this.getView().byId("table")
+        },
+
+        onPressRow: function(oEvent){
+           var index= oEvent.getParameter("rowIndex");
+           if(index === 0){
+            this.getView().byId("buttonDelete").setProperty("enabled", false)
+           }else{
+            this.getView().byId("buttonDelete").setProperty("enabled", true)
+           }
         },
 
         navToHome: function() {
