90da0be7ee9944c4e1197ea5896cf5c6a317ef35
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2025-02-05 13:51:01 +0100

    format Data ricezione

--- webapp/controller/Master3.controller.js
+++ webapp/controller/Master3.controller.js
@@ -41,6 +41,7 @@ sap.ui.define(
                 "sap/ui/demo/fiori2/mockdata/dataMaster3.json"
               )
             );
+            
             this.getOwnerComponent().setModel(oMaster3Model, "master3");
 
             break;
