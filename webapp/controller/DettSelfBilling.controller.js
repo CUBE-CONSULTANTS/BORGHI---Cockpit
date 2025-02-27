5c40192c271364fd39f13e488fe1e75b6983376c
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2025-02-28 00:57:44 +0100

    prova tree table selfBilling

--- webapp/controller/DettSelfBilling.controller.js
+++ webapp/controller/DettSelfBilling.controller.js
@@ -18,8 +18,8 @@ sap.ui.define(
         this._id =oEvent.getParameter("arguments").id || this._id || "0";
         let datiElementoSelect = this.getOwnerComponent().getModel("master3SB").getProperty("/").find((x) => (x.id = this._id));
         datiElementoSelect.dettaglio_fattura.forEach(pos=> {
-          pos.data_fattura = formatter.returnDate(pos.data_fattura,"yyyyMMdd","dd/MM/yyyy")
-          pos.data_scadenza_fattura = formatter.returnDate(pos.data_scadenza_fattura,"yyyyMMdd","dd/MM/yyyy")
+          pos.data_fattura = formatter.formatDate(pos.data_fattura)
+          pos.data_scadenza_fattura = formatter.formatDate(pos.data_scadenza_fattura)
           pos.riferimento_ddt = Object.values(pos.riferimento_ddt.results);
         })
         this.getView().setModel(
@@ -27,6 +27,7 @@ sap.ui.define(
           "detailData"
         );
         this.getModel("detailData").setProperty("/DettaglioMaster3", datiElementoSelect);
+        this.getModel("detailData").setProperty("/DettaglioFatture", datiElementoSelect.dettaglio_fattura);
         // this._registerForP13n();
         },
         
