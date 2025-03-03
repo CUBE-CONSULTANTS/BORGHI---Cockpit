9e471739237c9ca1b7800f165f2771af3ea9f637
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2025-03-03 11:14:26 +0100

    up Engine self billing

--- webapp/controller/DettSelfBilling.controller.js
+++ webapp/controller/DettSelfBilling.controller.js
@@ -15,28 +15,28 @@ sap.ui.define(
         },
 
         _onProductMatched: function (oEvent) {
-        this._id =oEvent.getParameter("arguments").id || this._id || "0";
-        let datiElementoSelect = this.getOwnerComponent().getModel("master3SB").getProperty("/").find((x) => (x.id = this._id));
-        datiElementoSelect.dettaglio_fattura.forEach(pos=> {
-          pos.data_fattura = formatter.formatDate(pos.data_fattura)
-          pos.data_scadenza_fattura = formatter.formatDate(pos.data_scadenza_fattura)
-          pos.riferimento_ddt = Object.values(pos.riferimento_ddt.results);
-          pos.riferimento_ddt.forEach(posit=>{
-            posit.data_ddt_cliente = formatter.formatDate(posit.data_ddt_cliente)
+          this._id =oEvent.getParameter("arguments").id || this._id || "0";
+          let datiElementoSelect = this.getOwnerComponent().getModel("master3SB").getProperty("/").find((x) => (x.id = this._id));
+          datiElementoSelect.dettaglio_fattura.forEach(pos=> {
+            pos.data_fattura = formatter.formatDate(pos.data_fattura)
+            pos.data_scadenza_fattura = formatter.formatDate(pos.data_scadenza_fattura)
+            pos.riferimento_ddt = Object.values(pos.riferimento_ddt.results);
+            pos.riferimento_ddt.forEach(posit=>{
+              posit.data_ddt_cliente = formatter.formatDate(posit.data_ddt_cliente)
+            })
           })
-        })
-        this.getView().setModel(
-          new JSONModel(),
-          "detailData"
-        );
-        this.getModel("detailData").setProperty("/DettaglioMaster3", datiElementoSelect);
-        this.getModel("detailData").setProperty("/DettaglioFatture", datiElementoSelect.dettaglio_fattura);
-        // this._registerForP13n();
+          this.getView().setModel(
+            new JSONModel(),
+            "detailData"
+          );
+          this.getModel("detailData").setProperty("/DettaglioMaster3", datiElementoSelect);
+          this.getModel("detailData").setProperty("/DettaglioFatture", datiElementoSelect.dettaglio_fattura);
+          this._registerForP13n(oEvent, "tablePosSB")
         },
         onCollapseAll: function (oEvent) {
           debugger
           let oTable = oEvent.getSource().getParent().getParent()
-              oTable.collapseAll();
+            oTable.collapseAll();
         },
         onExpandFirstLevel: function (oEvent) {
           debugger
