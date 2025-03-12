6cca8dbcec8938bee2a32aeef7391618e1dd9f9f
FrankieKnight <frankie.na08@gmail.com>
2025-03-12 10:45:05 +0100

    matchcode

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -7,43 +7,59 @@ sap.ui.define(
     "sap/m/MessageBox",
     "../model/models",
     "../model/API",
-	"sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
   ],
-  function (BaseController,
-	Filter,
-	FilterOperator,
-	Sorter,
-	MessageBox,
-	models,
-	API,
-	JSONModel) {
+  function (
+    BaseController,
+    Filter,
+    FilterOperator,
+    Sorter,
+    MessageBox,
+    models,
+    API,
+    JSONModel
+  ) {
     "use strict";
 
     return BaseController.extend("programmi.consegne.edi.controller.Master", {
       onInit: function () {
         this.setModel(models.createMainModel(), "main");
-        this.getRouter().getRoute("master").attachPatternMatched(this._onObjectMatched, this);
+        this.getRouter()
+          .getRoute("master")
+          .attachPatternMatched(this._onObjectMatched, this);
       },
-      _onObjectMatched: function (oEvent) {      
-        if(oEvent.getParameters().arguments.prevApp === 'monitor') {
-          this.getModel("main").setProperty("/backToMon", true)
-        }else if(oEvent.getParameters().arguments.prevApp === 'archivio'){
-          this.getModel("main").setProperty("/backToArch", true)
-        }else{
+      _onObjectMatched: async function (oEvent) {
+        if (oEvent.getParameters().arguments.prevApp === "monitor") {
+          this.getModel("main").setProperty("/backToMon", true);
+        } else if (oEvent.getParameters().arguments.prevApp === "archivio") {
+          this.getModel("main").setProperty("/backToArch", true);
+        } else {
           this.getModel("main").setProperty("/backToMon", false);
           this.getModel("main").setProperty("/backToArch", false);
         }
+
+        let model = this.getOwnerComponent().getModel("modelloV2");
+        let clienti = await API.getEntity(model, "/T661W", [], []);
+        this.getView().setModel(new JSONModel(), "matchcode");
+        this.getView()
+          .getModel("matchcode")
+          .setProperty("/clienti", clienti.results);
       },
-      onSearch: function (oEvent) {      
-      //  /delivery-forecast/EIGHTWEEK_MAT?$expand=WEEKS&$filter=CLIENTE eq '0000200137' and KDMAT eq 'BRESSANS_2ND_TEST'
-        let aFilters = []
-        this._searchVarArticolo(aFilters)
+      onSearch: function (oEvent) {
+        //  /delivery-forecast/EIGHTWEEK_MAT?$expand=WEEKS&$filter=CLIENTE eq '0000200137' and KDMAT eq 'BRESSANS_2ND_TEST'
+        let aFilters = [];
+        this._searchVarArticolo(aFilters);
       },
       _searchVarArticolo: async function (aFilters) {
-        debugger
+        debugger;
         try {
-          this.showBusy(0)
-          let weekCall = await API.getEntity(this.getOwnerComponent().getModel("modelloV2"), "/EIGHTWEEK_MAT", aFilters, ['WEEKS'])         
+          this.showBusy(0);
+          let weekCall = await API.getEntity(
+            this.getOwnerComponent().getModel("modelloV2"),
+            "/EIGHTWEEK_MAT",
+            aFilters,
+            ["WEEKS"]
+          );
           let formattedData = weekCall.results.map((item) => {
             return {
               CLIENTE: item.CLIENTE,
@@ -57,19 +73,18 @@ sap.ui.define(
                 WEEK: week.WEEK.trim(),
                 QTY: week.QTY.trim(),
                 PERC: week.PERC.trim(),
-              }))
+              })),
             };
           });
-          this.setModel(new JSONModel(formattedData),"variazioneArticolo" )
-          this.getModel("main").setProperty("/visibility",true)
+          this.setModel(new JSONModel(formattedData), "variazioneArticolo");
+          this.getModel("main").setProperty("/visibility", true);
         } catch (error) {
-          MessageBox.error("Errore durante la ricezione dei dati ",error)
-        }finally {
+          MessageBox.error("Errore durante la ricezione dei dati ", error);
+        } finally {
           this.hideBusy(0);
         }
       },
-      onSort: function () {     
-      },
+      onSort: function () {},
 
       onListItemPress: function (oEvent) {
         //apri dettaglio
--- webapp/controller/Master2.controller.js
+++ webapp/controller/Master2.controller.js
@@ -1,29 +1,38 @@
 sap.ui.define(
-  ["./BaseController", "sap/ui/model/json/JSONModel", "../model/models"],
-  function (BaseController, JSONModel, models ) {
+  [
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "../model/models",
+    "../model/API",
+  ],
+  function (BaseController, JSONModel, models, API) {
     "use strict";
 
     return BaseController.extend("programmi.consegne.edi.controller.Master2", {
       onInit: function () {
         this.setModel(models.createMainModel(), "main");
-        this.getRouter().getRoute("master2").attachPatternMatched(this._onObjectMatched, this);
-  
+        this.getRouter()
+          .getRoute("master2")
+          .attachPatternMatched(this._onObjectMatched, this);
       },
-      _onObjectMatched: function (oEvent) { 
-        
-        if(oEvent.getParameters().arguments.prevApp === 'monitor') {
-          this.getModel("main").setProperty("/backToMon", true)
-        }else if(oEvent.getParameters().arguments.prevApp === 'archivio'){
-          this.getModel("main").setProperty("/backToArch", true)
-        }else{
+      _onObjectMatched: async function (oEvent) {
+        if (oEvent.getParameters().arguments.prevApp === "monitor") {
+          this.getModel("main").setProperty("/backToMon", true);
+        } else if (oEvent.getParameters().arguments.prevApp === "archivio") {
+          this.getModel("main").setProperty("/backToArch", true);
+        } else {
           this.getModel("main").setProperty("/backToMon", false);
           this.getModel("main").setProperty("/backToArch", false);
         }
+
+        let model = this.getOwnerComponent().getModel("modelloV2");
+        let clienti = await API.getEntity(model, "/T661W", [], []);
+        this.getView().setModel(new JSONModel(), "matchcode");
+        this.getView()
+          .getModel("matchcode")
+          .setProperty("/clienti", clienti.results);
       },
-      onSearch: function (oEvent) {
-      
-      },
-      
+      onSearch: function (oEvent) {},
     });
   }
 );
