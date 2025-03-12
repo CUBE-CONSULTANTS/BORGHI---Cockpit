0b48973b9cdc3879a0a03b9c463987d72378e363
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2025-03-12 13:27:19 +0100

    up secondo grafico

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -21,14 +21,29 @@ sap.ui.define(
         debugger
         let oDetailData = this.getModel("detailData").getData()
         let dataChart = []
+        let dataChartPercentuali = []
         oDetailData.WEEKS.forEach(function (weekData) {
             dataChart.push({
                 "Settimane": weekData.WEEK,  
                 "Valore Ricavo": parseFloat(weekData.QTY) 
             })
+            if (weekData.PREV_QTY && weekData.QTY) {
+              let percentuale = ((weekData.QTY - weekData.PREV_QTY) / weekData.PREV_QTY) * 100;
+              dataChartPercentuali.push({
+                  "Settimane": weekData.WEEK,
+                  "Variazione Percentuale": percentuale
+              });
+          } else {
+              dataChartPercentuali.push({
+                  "Settimane": weekData.WEEK,
+                  "Variazione Percentuale": 0 
+              });
+          }
         })
+        
         oDetailData.ProductCollection = [{
-            variazioni_quantita: dataChart
+          variazioni_quantita: dataChart,
+          variazioni_percentuali: dataChartPercentuali
         }]
         this.getModel("detailData").setData(oDetailData);
     },
